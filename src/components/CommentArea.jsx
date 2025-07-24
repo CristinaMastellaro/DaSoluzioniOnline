442cd08ab0cc70175eca7735832f4ab3cb015a2a
CristinaMastellaro <cristinamastellaro8@gmail.com>
2025-07-24 16:30:15 +0200

    second extra

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -9,34 +9,35 @@ class CommentArea extends Component {
 
   endpoint = "https://striveschool-api.herokuapp.com/api/comments/";
 
-  retrieveComments = (id) => {
-    fetch(this.endpoint + id, {
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODcwYzQ2NDc4Y2RkZjAwMTU1ZDY3YTMiLCJpYXQiOjE3NTIyMjA3NzIsImV4cCI6MTc1MzQzMDM3Mn0.aps4Stj2AnXuaQyOjapUZvfSF2nL9WrlYcWeD7tpoDE",
-      },
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          throw new Error("Non siamo riusciti a caricare i commenti");
-        }
-      })
-      .then((commentsRetrieved) => {
-        this.setState({ comments: commentsRetrieved });
-      })
-      .catch((err) => console.log("Errore!", err));
-  };
+//   retrieveComments = (id) => {
+//     fetch(this.endpoint + id, {
+//       headers: {
+//         Authorization:
+//           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODcwYzQ2NDc4Y2RkZjAwMTU1ZDY3YTMiLCJpYXQiOjE3NTIyMjA3NzIsImV4cCI6MTc1MzQzMDM3Mn0.aps4Stj2AnXuaQyOjapUZvfSF2nL9WrlYcWeD7tpoDE",
+//       },
+//     })
+//       .then((res) => {
+//         if (res.ok) {
+//           return res.json();
+//         } else {
+//           throw new Error("Non siamo riusciti a caricare i commenti");
+//         }
+//       })
+//       .then((commentsRetrieved) => {
+//         this.setState({ comments: commentsRetrieved });
+//       })
+//       .catch((err) => console.log("Errore!", err));
+//   };
 
-  componentDidMount() {
-    this.retrieveComments(this.props.id);
-  }
+//   componentDidMount() {
+//     this.retrieveComments(this.props.id);
+//   }
 
   render() {
     return (
       <div>
-      <CommentsList commentsArray={this.state.comments} endpoint={this.endpoint}/>
+      {/* <CommentsList commentsArray={this.state.comments} endpoint={this.endpoint} id={this.props.id}/> */}
+      <CommentsList endpoint={this.endpoint} id={this.props.id}/>
       <AddComment id={this.props.id} endpoint={this.endpoint}/>
       </div>
     );
--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,8 +1,42 @@
 import { Component } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 
 class CommentsList extends Component {
+    
+    state = {
+        isLoading: true,
+        totalComments: [],
+    }
+
+    retrieveComments = () => {
+        fetch(this.props.endpoint + this.props.id, {
+        headers: {
+            Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODcwYzQ2NDc4Y2RkZjAwMTU1ZDY3YTMiLCJpYXQiOjE3NTIyMjA3NzIsImV4cCI6MTc1MzQzMDM3Mn0.aps4Stj2AnXuaQyOjapUZvfSF2nL9WrlYcWeD7tpoDE",
+        },
+        })
+        
+        .then((res) => {
+            if (res.ok) {
+            return res.json();
+            } else {            
+            throw new Error("Non siamo riusciti a caricare i commenti");
+            }
+        })
+        .then((commentsRetrieved) => {
+            this.setState({totalComments: commentsRetrieved, isLoading: false });
+            console.log("commentsRetrieved", commentsRetrieved)
+        })
+        .catch((err) => {
+            this.setState({isLoading: false}),
+            console.log("Errore!", err)
+        });
+    };
+
+  componentDidMount() {
+    this.retrieveComments();
+  }
     
     deleteReview = (endpointReview) => {
         fetch(endpointReview, {
@@ -17,18 +51,24 @@ class CommentsList extends Component {
             console.log("endpoint", this.props.endpoint)
             if (res.ok) {
                 alert("Commento cancellato!")
+                this.setState({isLoading: false})
+                this.retrieveComments();
             } else {
                 throw new Error("Non siamo riusciti a cancellare il commento")
             }
         })
-        .catch(err => {console.log("Errore!", err)})
+        .catch(err => {
+            this.setState({isLoading: false})
+            console.log("Errore!", err)
+        })
     }
     
     render () {
         // console.log("endpoint", this.props.endpoint)
         return(
             <ListGroup>
-                {this.props.commentsArray.map(comment => 
+                {this.state.isLoading && <Spinner variant="danger"/>}
+                {this.state.totalComments.map(comment => 
                     { 
                         // console.log("comment", comment)
                         return(                        
